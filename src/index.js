ea010f29c6fb41a048ffb522cb88093c8e45484a
Hamayun Saeed <hamayunsaeed48@gmail.com>
2024-09-27 11:48:19 -0700

    add utils

--- src/index.js
+++ src/index.js
@@ -1,9 +1,39 @@
 import connectDb from "./db/index.js";
 import dotenv from "dotenv";
+import {app} from "./app.js";
+import cors from "cors";
+import express from "express";
+import cookieParser from "cookie-parser";
 
 dotenv.config({
     path:"./.env"
-})
+});
+
+app.use(cors({
+    origin:process.env.CORS_ORIGIN,
+    credentials:true
+}));
+
+app.use(express.json({
+    limit:"16kb"
+}));
+
+app.use(express.urlencoded({extended:true,limit:"16kb"}));
+
+app.use(express.static("public"));
+app.use(express.cookieParser());
 
 
 connectDb()
+.then(()=>{
+    app.listen(process.env.PORT || 8000,()=>{
+        console.log(`server is running on port ${process.env.PORT}`)
+    });
+    app.on("error",(error)=>{
+        console.log("Error",error)
+        throw err
+    })
+})
+.catch((err)=>{
+    console.log("Mongodb connection fail:",err)
+})
---
+++ src/utils/ApiResponse.js
@@ -0,0 +1,10 @@
+class ApiResponse{
+    constructor(message ="Success",data,statusCode){
+        this.statusCode = statusCode
+        this.data = data
+        this.message=message
+        this.success = statusCode < 400
+    }
+}
+
+export{ApiResponse}
